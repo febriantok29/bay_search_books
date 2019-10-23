11073b2c2e3aff8df2e8190d3e07214f006ff155
Febrianto Kabisatullah <febriantok29>
2019-10-23 14:15:00 +0700

    Add slide right page route

---
+++ lib/app/navigations/SlideRightPageRoute.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+
+class SlideRightPageRoute extends PageRouteBuilder {
+  final WidgetBuilder builder;
+
+  SlideRightPageRoute({this.builder})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              builder(context),
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1.0, 0.0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          ),
+        );
+}
